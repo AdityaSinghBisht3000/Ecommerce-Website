813f380c78aba4faae2416ddf166ed49ba9b6d02
AdityaSinghBisht <adityasinghbisht1000001@gmail.com>
2024-03-23 07:57:00 +0530

    Project Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,50 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-
+import { useContext, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Category from "./components/Category/Category";
+import { Category as CategoryList } from "./components/Home/Category/Category";
+import { Category } from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
 import ReadMore from "./components/ReadMore/ReadMore";
+// import CategoryList from "./components/CategoryList/CategoryList";
+import { Context } from "./utils/context";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <AppContext>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/category/:id" element={<Category />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                    <Route path="/readmore" element={<ReadMore />} />
-                </Routes>
-                <Newsletter />
-                <Footer />
-            </AppContext>
-        </BrowserRouter>
-    );
+  const { categories, setCategories } = useContext(Context);
+
+  useEffect(() => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) => {
+      setCategories(res);
+    });
+  }, []);
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/category"
+            element={
+              <CategoryList
+                headingText={"Categories"}
+                categories={categories}
+              />
+            }
+          />
+          <Route path="category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="/readmore" element={<ReadMore />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  );
 }
 export default App;
